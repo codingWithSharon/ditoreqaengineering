d86dc411e2250d3e8551ce82efad3e28e98740a8
codingWithSharon <157753203+codingWithSharon@users.noreply.github.com>
2025-05-21 14:17:37 +0200

    Update Header.jsx

---
+++ my-portfolio/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <h1>Sharon Dito</h1>
+      <p>QA Engineer • Test Automation • Bug Hunter</p>
+      <nav>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

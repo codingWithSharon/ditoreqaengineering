62d0d545bfa1cc3f007ffc3716db1ccdd990a7a7
codingWithSharon <157753203+codingWithSharon@users.noreply.github.com>
2025-05-21 14:18:36 +0200

    Update Footer.jsx

---
+++ my-portfolio/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer id="contact">
+      <p>Get in touch: <a href="mailto:your.email@example.com">your.email@example.com</a></p>
+      <p>&copy; 2025 Sharon Dito</p>
+    </footer>
+  );
+}
+
+export default Footer;
